c287fe87ee35b33de54b574e4109f7cee7b19de4
Ritesh jha <128279306+R1I2T3@users.noreply.github.com>
2026-06-18 19:42:37 +0530

    fix personal workspace bug (#5)
    * fix personal workspace bug
    
    * chore: trigger CI

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -1,12 +1,15 @@
-import { auth } from "@Emitkit/auth";
+import type { auth as authInstance } from "@Emitkit/auth";
 import { db } from "@Emitkit/db";
 import type { Context as HonoContext } from "hono";
 
+export type Auth = typeof authInstance;
+
 export type CreateContextOptions = {
   context: HonoContext;
+  auth: Auth;
 };
 
-export async function createContext({ context }: CreateContextOptions) {
+export async function createContext({ context, auth }: CreateContextOptions) {
   const session = await auth.api.getSession({
     headers: context.req.raw.headers,
   });
